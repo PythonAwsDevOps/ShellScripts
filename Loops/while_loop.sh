a84d286ff8e9a86cb34a28f4d54d266171ca9454
Cloud User <ec2-user@ip-172-31-31-149.ec2.internal>
2018-01-10 09:03:32 +0000

    added while loop document

--- Loops/while_loop.sh
+++ Loops/while_loop.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
-
-#Method-1: simple while loop
+#Method-1: basic while loop
 i=0
 while [ $i -lt 100 ]
 do 
     echo "The i is : $i"
+    ((i++))
 done
-
-#Mehod-2: Infinity while loop
+#Mehod-2: while loop for infinity times
+<<mycomment
 while true
 do
     echo "enter ctrl + c to end"
 done
 
-#Method-3: Reading file with while loop
+#or simple in method dont increment i value, so that method one becomes infinity loop
+mycomment
+#Mehod-3: While loop for arrays
+myarray=(1 24 52 67 68)
+i=0
+while [ $i -lt 5 ]
+do
+   echo "my array value is: ${myarray[$i]}"
+   i=$(( i + 1 ))  
+done
+#Method-4: Reading file with while loop
+file_name="nari.txt"
 while read line
 do
     echo "The line is: $line"
-done < "file_name"
+done < "$file_name"
